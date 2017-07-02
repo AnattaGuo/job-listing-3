39f6503989251a8b242f281a735f314bfba60a80
AnattaGuo <mengjunkwok@gmail.com>
2017-07-03 07:29:06 +0800

    update public and hide function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  resources :jobs
+  root 'jobs#index'
+
   namespace :admin do
     resources :jobs do
       member do
@@ -9,10 +12,5 @@ Rails.application.routes.draw do
       end
     end
   end
-
-  resources :jobs
-  root 'jobs#index'
-
-
 
 end
