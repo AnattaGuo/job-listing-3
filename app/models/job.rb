c9d249fdecec62f42c35b6bc650395861e1a5c63
AnattaGuo <mengjunkwok@gmail.com>
2017-07-03 20:10:34 +0800

    step 6 order by wage

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,9 @@
 class Job < ApplicationRecord
-  validates :title, presence: true
+  scope :published, -> { where(is_hidden: false) }
+  scope :recent, -> { order('created_at DESC') }
 
+
+  validates :title, presence: true
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
